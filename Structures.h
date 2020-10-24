f1502dc0157ab0fd8e5482854387f0186280100b
Hendricks, Brent Andrew <brent.hendricks@p5software.com>
2020-10-24 10:01:02 -0400

    Added settings and comments

--- Structures.h
+++ Structures.h
@@ -1,3 +1,7 @@
+/*******************************************************************************/
+/*        Structures used within this program in section below                 */
+/*******************************************************************************/
+
 typedef struct structSettings {
   bool deviceIsProvisioned;
   String friendlyName;
@@ -37,16 +41,45 @@ typedef struct structSettings {
 
 typedef struct structLED {
   int pin;
-  String alias;
-  int color;
+  char name[24];
+  char color[20];
   int illumination;
   int style;
   int styleData;
-  int countOfDefinedIntensity;
+  int countOfDefinedBrightness;
 
-  struct structIntensity {
-    String alias;
+  struct structBrightness {
+    char name[20];
     int illumination;
-  } definedIntensity[8];
+  } definedBrightness[8];
 
 };
+
+/*******************************************************************************/
+/*        Simplified structures used for EEPROM I/O in section below           */
+/*******************************************************************************/
+
+typedef struct _structSettings{
+    char friendlyName[33]; //Max length: 32
+    char deviceName[17]; //Max length: 16
+
+    struct _structNetwork{
+      byte macAddress[6];
+      IPAddress ip;
+      IPAddress dns;
+      IPAddress gateway;
+      IPAddress subnet;
+    } network;
+
+    struct _structMqttServer{
+      char serverName[33]; //Max length: 32
+      unsigned int port;
+      char username[33]; //Max length: 32
+      char password[33]; //Max length: 32
+      char clientTopic[33]; //Max length: 32
+      char controlTopic[33]; //Max length: 32
+      char eventTopic[33]; //Max length: 32
+    } mqttServer;
+
+};
+
